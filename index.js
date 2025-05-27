ebb2e85b3a0e466eb73404d0e7f03cdc8f13a6e9
Ayush Shukla <ayushshukla8920@gmail.com>
2025-05-27 20:08:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/B2C', (req, res) => {
+  res.render('login');
+});
+
+app.get('/NOC/NOC25/SEM1/Ecertificates/106/noc25-cs11/Course/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(__dirname, 'public', filename);
+  res.sendFile(filePath, err => {
+    if (err) {
+      res.status(404).send('File not found');
+    }
+  });
+});
+
+// Route to handle the verification page
+app.get('/noc/Ecertificate', (req, res) => {
+    const certId = req.query.q;
+
+    if (!certId) {
+        return res.status(400).send("Missing certificate ID");
+    }
+
+    // Render button page
+    res.render('test', { certId });
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
