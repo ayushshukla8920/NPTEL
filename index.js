209af6d88164f340880636e3fac35ab3a0f8ebe9
Ayush Shukla <ayushshukla8920@gmail.com>
2025-05-27 20:12:02 +0530

    paths correctd

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = 3000;
 
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
